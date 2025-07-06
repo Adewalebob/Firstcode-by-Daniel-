5a52ba6ba783e7a495043c15e11ed825b9533039
Adewalebob <ajayidaniel248@gmail.com>
2025-07-07 00:09:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+<head>
+    <title>My first html page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Your content here -->
+    <script src="my first code"></script>
+</body>
+
